110137ec14864cc428a4c9821a1fbc201cd622cb
lifeifei <lifeifei@beescm.cn>
2019-08-01 14:52:53 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,51 @@
+# spring-boot-starter-seata
+## seata介绍
+[seata介绍](https://github.com/seata/seata)
+
+## 快速入门
+1、执行sql
+[业务数据表]
+DROP TABLE IF EXISTS `storage_tbl`;
+CREATE TABLE `storage_tbl` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `commodity_code` varchar(255) DEFAULT NULL,
+  `count` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`commodity_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `order_tbl`;
+CREATE TABLE `order_tbl` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(255) DEFAULT NULL,
+  `commodity_code` varchar(255) DEFAULT NULL,
+  `count` int(11) DEFAULT 0,
+  `money` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `account_tbl`;
+CREATE TABLE `account_tbl` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(255) DEFAULT NULL,
+  `money` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+[seata日志表]
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+2、复制配置文件[file.conf]、[registry.conf]文件至项目[resource]目录下
+
+3、使用全局事务注解 @GlobalTransactional(timeoutMills = 300000, name = "spring-cloud-demo-tx")
---
+++ spring-boot-starter-seata-sample/business-service/src/main/java/org/springframework/boot/seata/samples/businessservice/BusinessApplication.java
@@ -0,0 +1,51 @@
+package org.springframework.boot.seata.samples.businessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 
+ * @author zcb
+ * @date 2019年4月9日 上午10:29:19
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class BusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @FeignClient(value = "storage", url = "http://127.0.0.1:18082")
+    public interface StorageService {
+
+        @RequestMapping(path = "/storage/{commodityCode}/{count}")
+        String storage(@RequestParam("commodityCode") String commodityCode,
+                       @RequestParam("count") int count);
+
+        @RequestMapping(path = "/storageAdd/{commodityCode}/{count}")
+        String storageAdd(@RequestParam("commodityCode") String commodityCode,
+                       @RequestParam("count") int count);
+    }
+
+    @FeignClient(value = "order", url = "http://127.0.0.1:18083")
+    public interface OrderService {
+        @RequestMapping(path = "/order", method = RequestMethod.POST)
+        String order(@RequestParam("userId") String userId,
+                     @RequestParam("commodityCode") String commodityCode,
+                     @RequestParam("orderCount") int orderCount);
+
+    }
+}
